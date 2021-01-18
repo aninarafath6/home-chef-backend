e5b579ff13c517d5b32df424e32bf1d030901c14
aninarafath6 <arafathanin6@gmail.com>
2021-01-18 23:48:16 +0530

    razorpay success state

--- helpers/user_helpers/orders/place_order.js
+++ helpers/user_helpers/orders/place_order.js
@@ -24,6 +24,7 @@ return new Promise(async(resolve,reject)=>{
 console.log({status: status,
   order_id: response.ops[0]._id,
   total: response.ops[0].totalAmount.total,});
+  console.log({orderResponse:response.ops[0]});
        resolve({
          status: status,
          order_id: response.ops[0]._id,
